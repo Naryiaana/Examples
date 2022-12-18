ae2d402026ccc85f9f2c870e006022ba5411da4f
Nariana <naryyaana@mail.ru>
2022-12-19 01:16:17 +0900

    new examples added

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 2, 3, 456, 16, 74, 456};
+
+int n = array.Length;
+int find = 456;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    index++;
+}
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection) //метод заполнения массива
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //метод вывода
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //искусственный прием, если значения нет, то по умолчанию -1
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int[] array = new int[10]; //создает массив, в котором 10 элементов, по умолчанию он наполнен нулями
+
+FillArray(array);
+array[4] = 4; //принудительно добавляем 4ки для проверки после отрбатки метода
+array[5] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 44);
+Console.WriteLine(pos);
